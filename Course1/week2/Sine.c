b2b317aa03e7162455b5daccab2599c8f87588c1
Simona <90156196+simonapopovici10@users.noreply.github.com>
2023-08-01 01:14:16 +0300

    Add files via upload

---
+++ Course1/week2/Sine.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159265
+
+int main(void)
+{
+
+    double number, val, sine;
+
+    printf("Enter a number:"); //The given number represent the number of degrees
+    scanf("%lf", &number);
+
+    val = PI / 180;           //Converts degrees into radians
+    sine = sin(number * val); // Calculates the sine of the given number which was converted from degrees into radians
+
+    printf("The sine if %lf is %lf", number, sine); //Printing the sine of the number given
+
+    return 0;
+}
