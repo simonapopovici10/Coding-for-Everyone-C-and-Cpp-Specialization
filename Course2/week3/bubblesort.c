cc8688c8cc5abfa56eface641c458e9bb5931f0c
Simona <90156196+simonapopovici10@users.noreply.github.com>
2023-08-01 01:56:37 +0300

    Update bubblesort.c

--- Course2/week3/bubblesort.c
+++ Course2/week3/bubblesort.c
@@ -1,3 +1,12 @@
+/*Use the linear linked list code to store a randomly generated set of 100 integers.  
+Now write a routine that will rearrange the list in sorted order of these values. 
+Note you might want to use bubble sort to do this. Print these values in rows of 5 on the screen.
+
+How will this work? Compare two adjacent list elements and if they are out of order swap them. 
+After a first pass the largest element will have bubbled to the end of the list. 
+Each pass can look at one less element as the end of a list stays sorted.*/
+
+
 #include<stdio.h> 
 #include<stdlib.h> 
   
