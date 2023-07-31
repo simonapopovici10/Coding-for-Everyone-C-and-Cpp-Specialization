43f062479fa64d51870240097f45e5bb529e8363
Simona <90156196+simonapopovici10@users.noreply.github.com>
2023-08-01 01:12:14 +0300

    Add files via upload

---
+++ Course1/week1/poem.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    printf("Wild nights - Wild nights\nWere I with thee\nWild nights should be\nOur luxury!\nFutile - the winds -\nTo a Heart in port -\nDone with the Compass -\nDone with the Chart!\nRowing in Eden -\nAh - the Sea!\nMight I but moor - tonight -\nIn thee!\n");
+
+    return 0;
+}
