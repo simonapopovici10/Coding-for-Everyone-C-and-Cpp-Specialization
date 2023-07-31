949354c765f562bcd14ec05a9d21a19683414f13
Simona <90156196+simonapopovici10@users.noreply.github.com>
2023-08-01 01:52:00 +0300

    Update usingStruct.c

--- Course2/week2/usingStruct.c
+++ Course2/week2/usingStruct.c
@@ -1,4 +1,9 @@
-//Write enumerated types that supports dates—such as december 12. Then add a function that produces a next day.  So nextday(date) of december 12 is december 13. Also write a function printdate(date) that prints a date legibly.The function can assume that February has 28 days and it most know how many days are in each month. Use a struct with two members; one is the month and the second  is the day of the month—an int (or short).
+/* Write enumerated types that supports dates—such as december 12. 
+Then add a function that produces a next day.  
+So nextday(date) of december 12 is december 13. 
+Also write a function printdate(date) that prints a date legibly.
+The function can assume that February has 28 days and it most know how many days are in each month. 
+Use a struct with two members; one is the month and the second  is the day of the month—an int (or short).*/
 
 
 #include <stdio.h>
